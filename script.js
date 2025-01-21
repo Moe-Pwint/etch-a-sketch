177671d80519c4c71014a1b43b5b7bfadff5e29f
Moe Pwint <moezatmoepwint@gmail.com>
2025-01-21 22:51:55 +0700

    Add clickEvent to Button & createGrid function When the button is clicked, it prompts userInput. Then, the userInput is used as argument to function createGrid.

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 
-function createGrid(number) {
+const getNum = document.querySelector('#getNum')
+const container = document.querySelector('#container');
 
-    const container = document.querySelector('#container');
+getNum.addEventListener('mousedown', () => {container.textContent = '';});
+
+getNum.addEventListener('click',() => {
+    
+    let wrongInput = true;
+    while (wrongInput) {
+        
+        const answer = prompt('Please type in a number between 10 and 100');
+        console.log(answer);
+        if (answer >= 10 && answer <= 100) {
+            console.log(createGrid(answer));
+            wrongInput = false;
+            break;
+        } else {
+            alert('Please type a valid number!');
+        }}
+    });
+
+
+function createGrid(number) {
     for (let i = 0; i < number; i++) {
         let col = document.createElement("div");
         col.setAttribute('class','column');
 
         for (let j = 0; j < (number); j++) {
-            let squareDiv = document.createElement('div');
+            const squareDiv = document.createElement('div');
             squareDiv.setAttribute("class","squareDiv");
             col.appendChild(squareDiv);
         } container.appendChild(col);
-    }
+
+    } const pixel = document.querySelectorAll('.squareDiv');
+        pixel.forEach(p => {
+        p.addEventListener('mouseenter', () => {
+            p.style.backgroundColor = 'darkblue';
+        p.addEventListener('mouseleave', () => {
+            p.style.backgroundColor = 'lightblue';
+    })
+    })
+});
 }
 
 
-console.log(createGrid(4));
+
+
+/*
+Rather than squares being the same color throughout the grid, 
+randomize the squares’ RGB values with each interaction.
+*/
+
+
+/*
+
+    */
